66841026dd0022604fb6a91bf4d3a53efad09b11
dmaiocchi <dmaiocchi@suse.com>
2019-06-21 21:01:24 +0200

    implement some minimal structure

--- src/cosmic_river/core.clj
+++ src/cosmic_river/core.clj
@@ -1,6 +1,16 @@
-(ns cosmic-river.core)
+(ns cosmic-river.core
+  (:require [tentacles.core]
+            [tentacles.events]
+            [tentacles.repos])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn github-events []
+  (clojure.edn/read-string (slurp "github-events.edn")))
+
+(defn get-all-events []
+  (get-in (github-events) [:github-events]))
+
+(defn get-repo-public-events [user repo]
+  (tentacles.events/repo-events)
+)
