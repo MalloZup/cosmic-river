8f9a84f1c886771f1a81854d976e74f18515edef
dmaiocchi <dmaiocchi@suse.com>
2019-06-25 15:36:56 +0200

    Implement rabbitmq basic models and add a client

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 Listen to GitHub V3 events and dispatch them via a message-broker of choice.
 
+# Rationale:
+
+Having event-driven automation: react to GitHub events and automate your organisation workflows with a universal simplicity.
+
+# Layout of repo:
+
+This repository contains basically two clojure projects.
+
+- cosmic-river (server)
+- criver-client
+
+Cosmic-river server aims to distribute the GitHub events to the criver-clients
+
+The criver-clients will consume these events, filtering them and triggering the automation needed.
+
 # Roadmap:
 
 See if a repo release and trigger event ( chat, etc)
@@ -10,7 +25,6 @@ See if a repo release and trigger event ( chat, etc)
 - implement message broker functionalities
 
 (client-side)
-
 filter the events of repo
 
 
--- src/cosmic_river/rabbitmq.clj
+++ src/cosmic_river/rabbitmq.clj
@@ -1,25 +1,38 @@
 (ns cosmic-river.rabbitmq
-   (:gen-class)
-   (:require [langohr.core     :as rmq]
+  (:gen-class)
+  (:require [langohr.core      :as rmq]
             [langohr.channel   :as lch]
             [langohr.queue     :as lq]
+            [langohr.exchange  :as le]
             [langohr.consumers :as lc]
             [langohr.basic     :as lb]))
 
-(def default-exchange-name "")
 
-(defn message-handler [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
-  (println (format "[consumer] Received a message: %s, delivery tag: %d, content type: %s, type: %s" (String. payload "UTF-8") delivery-tag content-type type)))
+(defn start-consumer
+  "Starts a consumer bound to the given topic exchange in a separate thread"
+  [ch topic-name username]
+  (let [queue-name (format "nba.newsfeeds.%s" username)
+        handler    (fn [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
+                     (println (format "[consumer] %s received %s" username (String. payload "UTF-8"))))]
+    (lq/declare ch queue-name {:exclusive false :auto-delete true})
 
-(defn start  [& args]
+    (lc/subscribe ch queue-name handler {:auto-ack true})))
+
+(defn -main
+  [& args]
   (let [conn  (rmq/connect)
         ch    (lch/open conn)
-        qname "langohr.examples.hello-world"]
-    (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
-    (lq/declare ch qname {:exclusive false :auto-delete true})
-    (lc/subscribe ch qname message-handler {:auto-ack true})
-    (lb/publish ch default-exchange-name qname "Hello!" {:content-type "text/plain" :type "greetings.hi"})
+        ;; exchange name: Exchanges receive mesg that are sent by producers
+        ex    "nba.scores"]
+
+    ;; TODO: make this parametrizable in the edn as user input.
+    (le/declare ch ex "fanout" {:durable true :auto-delete false})
+    ;; START a consumer before publish
+    (lb/publish ch ex "" "BOS 101, NYK 89" {:content-type "text/plain" :type "scores.update"})
+    (lb/publish ch ex "" "ORL 85, ATL 88"  {:content-type "text/plain" :type "scores.update"})
     (Thread/sleep 2000)
-    (println "[main] Disconnecting...")
+    ;; consider the case that daemon is down and we couldn't close connection
     (rmq/close ch)
     (rmq/close conn)))
+
+
