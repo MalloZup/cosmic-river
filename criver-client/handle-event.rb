713e89c6e73bc8d20623ed58ef4826e9337c9c8c
dmaiocchi <dmaiocchi@suse.com>
2019-06-29 02:11:56 +0200

    experimental

---
+++ criver-client/handle-event.rb
@@ -0,0 +1,7 @@
+#! /usr/bin/ruby
+
+require 'json'
+
+#puts JSON.load(ENV['CRIVER_EVENTS_FPATH'])
+puts JSON.load("/tmp/branzino.json")
+
--- criver-client/src/criver_client/core.clj
+++ criver-client/src/criver_client/core.clj
@@ -8,6 +8,14 @@
 (use '[clojure.java.shell :only [sh]])
 (use 'clojure.java.io)
 
+
+(defn exec-cmd-and-pass-env-var [shell-command tmp-file-path]
+  (println (:out (sh shell-command :env {"CRIVER_EVENTS_FPATH" tmp-file-path}))))
+
+(defn dump-events-to-tmp-file [events-json tmp-file-name]
+ (with-open [wrtr (writer tmp-file-name)]
+ (.write wrtr events-json)))
+ 
 (defn client-edn-config []
   (clojure.edn/read-string (slurp "cr-rabbitmq.edn")))
 
@@ -17,13 +25,16 @@
   "Starts a consumer bound to the given topic exchange in a separate thread"
   (doseq [consumer (rabbitmq-consumers)]
    (let [qe-name (format "criver.%s" (:qe-name consumer))
-        conn  (rmq/connect)
+         conn  (rmq/connect)
         ex-name (:exchange-name consumer)
         ch    (lch/open conn)
         shell-command (:shell-command consumer)
+        tmp-file-path (str "/tmp/" "criver-" ex-name "-" (str (rand-int 90)) "-" qe-name "-" "events.json" )
         msg-handler  (fn [ch {:keys [content-type delivery-tag type] :as meta} ^bytes payload]
-                       ;; TODO: pass payload via environment var so it is accessible
-                       (println (:out (sh shell-command))))]
+                       (println payload)
+                       (dump-events-to-tmp-file (String. payload "UTF-8") tmp-file-path)
+                       (exec-cmd-and-pass-env-var shell-command tmp-file-path)
+                       )]
 
      
     (println (str "[DEBUG]: starting consumer with queue-name: " qe-name "and ex-name: " ex-name))
